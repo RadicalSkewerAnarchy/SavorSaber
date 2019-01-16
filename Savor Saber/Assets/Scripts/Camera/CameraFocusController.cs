24c363256a1db6ec789626c087a48e5bba456f11
Valentino Abate <valentinoabate1@gmail.com>
2019-01-16 15:56:32 -0800

    Merged in Camera and Player control from Kindon

---
+++ Savor Saber/Assets/Scripts/Camera/CameraFocusController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocusController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Target;
+    //
+    [SerializeField]
+    [Range(0f, 10f)]
+    float Snap;
+    //
+    [SerializeField]
+    [Range(0f, 10f)]
+    int RadiusChain;
+    //
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xTo;
+        float yTo;
+        
+        var leftRight = 1;
+        var upDown = 1;
+        var localPosition = transform.position;
+      
+
+        leftRight = DirectionX();
+        upDown = DirectionY();
+        xTo = Target.transform.position.x + leftRight * Mathf.Min((RadiusChain), Mathf.Abs(localPosition.x - Camera.main.ScreenToWorldPoint(Input.mousePosition).x));
+        yTo = Target.transform.position.y + upDown * Mathf.Min((RadiusChain), Mathf.Abs(localPosition.y - Camera.main.ScreenToWorldPoint(Input.mousePosition).y));
+
+        localPosition.x += (xTo - localPosition.x);
+        localPosition.y += (yTo - localPosition.y);
+        transform.position = Vector2.MoveTowards(transform.position, localPosition, Snap * Time.deltaTime);
+    }
+    int DirectionX()
+    {
+        var camCoord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(camCoord.x < Target.transform.position.x)
+        {
+            return -1;
+        }
+        return 1;
+    }
+    int DirectionY()
+    {
+        var camCoord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (camCoord.y < Target.transform.position.y)
+        {
+            return -1;
+        }
+        return 1;
+    }
+}
---
+++ Savor Saber/Assets/Scripts/Player/UpdatedController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatedController : MonoBehaviour
+{   
+    [SerializeField]
+    bool DebugBool = false;
+    //////
+    [SerializeField]
+    [Range(100f,500f)]
+    float speed;
+    //////
+    /*[SerializeField]
+    [Range(5f, 1000f)]
+    float deceleration;*/
+    //////
+    Rigidbody2D rigidBody;
+    Animator animatorBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animatorBody = GetComponent<Animator>();
+    }
+
+    /*Update is called once per frame*/
+    void FixedUpdate()
+    {   
+        /*Two functions are used here in order to allow easy edition of 
+        movement and stopping behavior as well as immediate frame by frame updates*/
+        MoveAgent();
+        //StopAgent();
+        AnimateAgent();
+    }
+
+    void MoveAgent()
+    {   
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movementVector = new Vector2(moveHorizontal, moveVertical);
+        
+        if (movementVector.magnitude > 1)
+        {
+            rigidBody.velocity = (movementVector/movementVector.magnitude * speed * Time.deltaTime);
+        }
+        else
+        {
+            rigidBody.velocity = (movementVector * speed * Time.deltaTime);
+        }
+        //////
+        if (DebugBool) { Debug.Log("MoveAgent finished."); }
+        //////
+        //https://forum.unity.com/threads/diagonal-movement-speed-to-fast.271703/, comment by cranky. Normalizing
+        //the vector adds a weird feel to the player but dividing by the vector's magnitude if it's greater than 1 
+        //gives a tight control feel
+    }
+
+    void StopAgent()
+    {   
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movementVector = new Vector2(moveHorizontal, moveVertical);
+        var negativeDirection = -rigidBody.velocity;
+ 
+        if (movementVector == Vector2.zero && rigidBody.velocity != Vector2.zero)
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            rigidBody.drag = 1;
+        }
+        //////
+        if (DebugBool) { Debug.Log("StopAgent finished."); }
+        //////
+    }
+
+    void AnimateAgent()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movementVector = new Vector2(moveHorizontal, moveVertical);
+        var movementAngle = Vector2.SignedAngle(Vector2.right, movementVector);
+
+        animatorBody.SetFloat("SpeedX", moveHorizontal);
+        animatorBody.SetFloat("SpeedY", moveVertical);
+        
+        //calculates angle agent is moving based on right vector(1,0) and agent movementVector
+        
+        if (movementAngle < 0)
+        {
+            movementAngle += 360;
+        }
+        if(movementVector != Vector2.zero)
+        {
+            animatorBody.SetBool("Walking", true);
+            if (movementAngle > 315 || movementAngle < 45)
+            {
+                animatorBody.SetFloat("LastMoveX", 1f);
+            }
+            else if (movementAngle > 135 && movementAngle < 225)
+            {
+                animatorBody.SetFloat("LastMoveX", -1F);
+            }
+            else
+            {
+                animatorBody.SetFloat("LastMoveX", 0f);
+            }
+            if(movementAngle > 45 && movementAngle < 135)
+            {
+                animatorBody.SetFloat("LastMoveY", 1f);               
+            }
+            else if (movementAngle > 225 && movementAngle < 315)
+            {
+                animatorBody.SetFloat("LastMoveY", -1f);                
+            }
+            else
+            {
+                animatorBody.SetFloat("LastMoveY", 0f);
+            }
+            
+            
+        }
+        else
+        {
+            animatorBody.SetBool("Walking", false);
+        }
+        //////
+        if (DebugBool) { Debug.Log("AnimateAgent finished."); }
+        //////
+    }
+}
+
+// used for basic movement implementaion, https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/controlling-player
