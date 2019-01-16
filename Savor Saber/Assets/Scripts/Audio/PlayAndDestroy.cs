8f17475f2316f9834042c1782bbba42288f8d1d6
Valentino Abate <valentinoabate1@gmail.com>
2019-01-16 04:35:18 -0800

    Added 3d spatial audio support and monster death noises
    -Monsters now have the "deathSfx field" that will play when they die
    -To properly play, the player must add an audioplayer with a "play and destroy" component. this component will be spawned on death
    -------------Utility/Testing-------------
    Added a basic script to move an object and a basic script to have the camera follow an object.

---
+++ Savor Saber/Assets/Scripts/Audio/PlayAndDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAndDestroy : MonoBehaviour
+{
+    public void Play(AudioClip toPlay)
+    {
+        GetComponent<AudioSource>().PlayOneShot(toPlay);
+        Destroy(gameObject, toPlay.length);
+    }
+}
--- Savor Saber/Assets/Scripts/Character/Monster.cs
+++ Savor Saber/Assets/Scripts/Character/Monster.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 /// <summary>
 /// Attatch this component to an object to make it a monster
-/// Monsters are required to have a Rigidboy2D and a DropOnDeath component
-/// Monsters should probably have a collider
 /// </summary>
-[RequireComponent(typeof(Rigidbody2D), typeof(DropOnDeath), typeof(SpriteRenderer))]
-[RequireComponent(typeof(Health))]
+[RequireComponent((typeof(Collider2D)), typeof(Rigidbody2D), typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource), typeof(Health), typeof(DropOnDeath))]
 public class Monster : MonoBehaviour
 {
     /// <summary>name of the monster in-game</summary>
     public string displayName;
+
+    public AudioClip deathSfx;
+    public GameObject deathSfxPlayer;
     
     private void Start()
     {
         var health = GetComponent<Health>();
-        var drop = GetComponent<DropOnDeath>();
-        //Drop items and die if health is 0 or lower
-        Health.HealthEvent onDeath = (hp) =>
-        {
-            if (hp <= 0)
-            {
-                drop.Drop();
-                Destroy(gameObject);
-            }
-        };
-        health.AddEvent(onDeath);
+        // Drop items and die if health is 0 or lower        
+        health.AddEvent((hp) => { if (hp <= 0) Kill(); });
+    }
+
+    public void Kill()
+    {
+        var deathSoundObj = Instantiate(deathSfxPlayer, transform.position, transform.rotation);
+        deathSoundObj.GetComponent<PlayAndDestroy>().Play(deathSfx);
+        GetComponent<DropOnDeath>().Drop();
+        Destroy(gameObject);
     }
 }
