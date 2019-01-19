35229d679e1bf23942e446dabd9cb6df1dbb9486
Valentino Abate <valentinoabate1@gmail.com>
2019-01-18 17:12:49 -0800

    Added AI data framework and utility-based state choice
    -Values are specified in AIData and CharacterData
    -State is chose by the UtilityCurves component
    -Current debug key for checking state is "u"
    TODO: add "Macro Values" functionality to simplify actual curve calculations

---
+++ Savor Saber/Assets/Scripts/Utility/AIData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIData : CharacterData
+{
+    #region Moods
+    public float fear;
+    public float hunger;
+    public float hostility;
+    public float friendliness;
+    #endregion
+    public delegate float GetNormalValue();
+    private Dictionary<string, GetNormalValue> _values;
+
+    private void Start()
+    {
+        _values = new Dictionary<string, GetNormalValue>()
+        {
+            {"fear", () => {return fear; } },
+            {"hunger", () => {return hunger; } },
+            {"hostility", () => {return hostility; } },
+            {"friendliness", () => {return friendliness; } },
+            //{"playerDistance", GetPlayerDist },
+            {"health", () => {return Normal(health, maxHealth); } }
+        };
+    }
+
+    public float Normal(int now, int max)
+    {
+        return now / (float)max;
+    }
+
+    public float getNormalizedValue(string value)
+    {
+        return _values[value]();
+    }
+}
--- Savor Saber/Assets/Scripts/Utility/UtilityCurves.cs
+++ Savor Saber/Assets/Scripts/Utility/UtilityCurves.cs
@@ -2,45 +2,60 @@
 using System.Collections.Generic;
 using SerializableCollections;
 using UnityEngine;
+using System.Linq;
 
+[RequireComponent(typeof(AIData))]
 public class UtilityCurves : MonoBehaviour
 {
-
     public AnimationCurve curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
     public AIStates aiStates = new AIStates();
-    //Replace with stats
-    public AIValues aiMoods = new AIValues();
-    public AIValues aiStats = new AIValues();
+    public AIData data;
 
     // Use this for initialization
     void Start()
     {
-        aiMoods.Add("Hostility", 0.5f);
-        aiMoods.Add("Fear", 0.5f);
-        aiMoods.Add("Hunger", 0.5f);
+        data = GetComponent<AIData>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Move later
+        if(Input.GetKeyDown(KeyCode.U))
+            Debug.Log("Picked state: " + decideState());
     }
 
-    private float EvaluateAttribute(AnimationCurve cur, int now, int max)
+    public string decideState()
     {
-        // proportion of now to max
-        //      0 <= ret <= 1
-        float ret = (float)now / (float)max;
+        //Dictionary<string, float> utilityValues = new Dictionary<string, float>();
+        string maxState = "";
+        float max = 0;
+        foreach(var kvp in aiStates)
+        {
+            Debug.Log("State: " + kvp.Key);
+            //utilityValues.Add(kvp.Key, SumCurves(kvp.Value));
+            float utility = SumCurves(kvp.Value);
+            Debug.Log(kvp.Key + " utility: " + utility);
+            if (utility > max)
+            {
+                max = utility;
+                maxState = kvp.Key;
+            }          
+        }
+        return maxState;
+    }
 
+    private float EvaluateAttribute(AnimationCurve cur, float value)
+    {
         // evaluate with respect to curve
         //      example: curve = (y = x^2), ret = 0.5, eva = 0.25
-        float eva = cur.Evaluate(ret);
+        float eva = cur.Evaluate(value);
 
         // return
         return eva;
     }
 
-    private float SumCurves(AnimationCurve[] curves, int[] attributes)
+    private float SumCurves(CurveDict curves)
     {
         // attributes is TWICE as long as curves
         // curves[i] ==> attributes[2i] and attributes[2i+1]
@@ -48,32 +63,32 @@ public class UtilityCurves : MonoBehaviour
 
         // return variables
         float sum = 0;
-        int len = curves.Length;
+        int count = 0;
 
         // loop variables
         AnimationCurve c;
-        int a, b;
+        float a;
 
         // extrapolate, iterate, sum
-        for (int i = 0; i < len; i++)
+        foreach(var curvePair in curves)
         {
-            c = curves[i];
-            a = attributes[2 * i];
-            b = attributes[(2 * i) + 1];
-            sum += EvaluateAttribute(c, a, b);
+            c = curvePair.Value;
+            a = data.getNormalizedValue(curvePair.Key.ToLower());
+            float val = EvaluateAttribute(c, a);
+            Debug.Log(curvePair.Key + " value: " + a + " weight: " + val);
+            if(val >= 0)
+            {
+                sum += val;//EvaluateAttribute(c, a);
+                ++count;
+            }            
         }
 
         // return the average
-        return (sum / len);
+        return count == 0 ? 0 : (sum / count);
     }
 
     [System.Serializable]
     public class AIStates : SDictionary<string, CurveDict> { }
     [System.Serializable]
     public class CurveDict : SDictionary<string, AnimationCurve> { }
-    [System.Serializable]
-    public class AIValues : SDictionary<string, float>
-    {
-        
-    }
 }
