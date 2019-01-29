0df0d5fd6824b483c6b2ef70751b86565a9e0330
Valentino Abate <valentinoabate1@gmail.com>
2019-01-28 16:09:20 -0800

    Added some more test map stuff

---
+++ Savor Saber/Assets/Scripts/Camera/PixelPerfectZoomer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.U2D.PixelPerfectCamera))]
+public class PixelPerfectZoomer : MonoBehaviour
+{
+    private UnityEngine.U2D.PixelPerfectCamera z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        z = GetComponent<UnityEngine.U2D.PixelPerfectCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            z.assetsPPU += 1;
+        if (Input.GetKeyDown(KeyCode.O))
+            z.assetsPPU -= 1;
+    }
+}
--- Savor Saber/Assets/Scripts/Player/UpdatedController.cs
+++ Savor Saber/Assets/Scripts/Player/UpdatedController.cs
@@ -52,7 +52,6 @@ public class UpdatedController : MonoBehaviour
 
     void Start()
     {
-        animatorBody.GetCurrentAnimatorStateInfo()
         rigidBody = GetComponent<Rigidbody2D>();
         animatorBody = GetComponent<Animator>();
     }
