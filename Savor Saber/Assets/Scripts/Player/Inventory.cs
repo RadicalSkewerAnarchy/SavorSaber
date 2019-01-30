5b9347ba9a6dd9037e140e25a0f3d496939de82e
Lithiumantis <juyyek@ucsc.edu>
2019-01-30 15:58:29 -0800

    Revised control scheme
    J/K/L to slash, skewer, shoot
    
    Space to cook

--- Savor Saber/Assets/Scripts/Player/Inventory.cs
+++ Savor Saber/Assets/Scripts/Player/Inventory.cs
@@ -22,7 +22,7 @@ public class Skewer
 
     public void PushIngredient(IngredientData ingredient)
     {
-        ingredientStack.Push(ingredient);
+        ingredientStack.Push(ingredient);   
     }
 
     public IngredientData PopIngredient()
@@ -236,7 +236,7 @@ public class Inventory : MonoBehaviour {
     private void GetCookingInput()
     {
         //Press C to cook
-        if (Input.GetKeyDown(KeyCode.C) && nearCampfire)
+        if (Input.GetButtonDown("Cook") && nearCampfire)
         {
             if (quiver[activeSkewer].GetCount() > 0)
             {
@@ -247,7 +247,7 @@ public class Inventory : MonoBehaviour {
                 Debug.Log("Your inventory is empty, cannot cook");
             }
         }
-        else if (Input.GetKeyDown(KeyCode.C) && !nearCampfire)
+        else if (Input.GetButtonDown("Cook") && !nearCampfire)
         {
             if (quiver[activeSkewer].GetCount() > 0)
             {
