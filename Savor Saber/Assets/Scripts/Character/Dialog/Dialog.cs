4b6c56b1d32f0bc542f4fa03b3934414d1436d46
Coppermantis <juyyek@ucsc.edu>
2019-02-02 22:11:15 -0800

    Dialog Box Prefab
    - Created prefab for dialog boxes and script to manage them
    
    - Changed WasterMaster font from Unicode to Dynamic so it can be scaled with Best Fit setting
    
    -

---
+++ Savor Saber/Assets/Scripts/Character/Dialog/Dialog.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    #region fields
+
+    public Sprite[] portraitSprites;
+
+    public string[] currentSpeaker;
+
+    public string[] text;
+
+    public GameObject dialogBoxPrefab;
+    public Canvas UICanvas;
+
+
+    private GameObject dialogBox;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(dialogBox.transform.localPosition);
+    }
+
+    public void Activate()
+    {
+        dialogBox = Instantiate(dialogBoxPrefab, Vector3.zero, Quaternion.identity);
+        dialogBox.transform.SetParent(UICanvas.transform);
+        RectTransform rectTransform = dialogBox.GetComponent<RectTransform>();
+        dialogBox.transform.localScale = new Vector3(1, 1, 1);
+        rectTransform.anchoredPosition = new Vector3(0, 50, 0);
+    }
+}
