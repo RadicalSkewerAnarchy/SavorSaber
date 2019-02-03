1c69b8f728b45001a7986810e1832a6a10adb9a0
Coppermantis <juyyek@ucsc.edu>
2019-02-02 23:09:29 -0800

    Dialog System
    - Completed basic system for displaying dialog on screen
    
    - Portraits and text can be set on any object with the Dialog script component. Call its Activate() function to begin dialog.

--- Savor Saber/Assets/Scripts/Character/Dialog/Dialog.cs
+++ Savor Saber/Assets/Scripts/Character/Dialog/Dialog.cs
@@ -7,6 +7,14 @@ public class Dialog : MonoBehaviour
 {
     #region fields
 
+    /// <summary>
+    /// Can the dialog be repeated?
+    /// </summary>
+    public bool repeatable = false;
+
+    /// <summary>
+    /// Arrays for the dialog content itself
+    /// </summary>
     public Sprite[] portraitSprites;
 
     public string[] currentSpeaker;
@@ -18,28 +26,74 @@ public class Dialog : MonoBehaviour
 
 
     private GameObject dialogBox;
+    private Text dialogText;
+    private Image dialogImage;
+    
+    private int stage = 0;
+    private bool active = false;
 
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cook") && active)
+        {
+            stage++;
+            Debug.Log("Advancing to dialog stage " + stage);
+            if(stage >= text.Length)
+            {
+                Destroy(dialogBox);
+                if (!repeatable)
+                    Destroy(this.gameObject);
+            }
+            else
+            {
+                NextDialog();
+            }
 
+        }
     }
 
     public void Activate()
     {
+        Debug.Log("Activating dialog");
+        stage = 0;
+        //set position on of dialog box on screen
         dialogBox = Instantiate(dialogBoxPrefab, Vector3.zero, Quaternion.identity);
         dialogBox.transform.SetParent(UICanvas.transform);
         RectTransform rectTransform = dialogBox.GetComponent<RectTransform>();
         dialogBox.transform.localScale = new Vector3(1, 1, 1);
         rectTransform.anchoredPosition = new Vector3(0, 50, 0);
+
+        Debug.Log("stage: " + stage);
+        dialogText = dialogBox.GetComponentInChildren<Text>();
+        dialogText.text = text[stage];
+
+        Transform portrait = dialogBox.transform.GetChild(1);
+        dialogImage = portrait.GetComponent<Image>();
+        dialogImage.sprite = portraitSprites[stage];
+
+        StartCoroutine(Wait(0.5f));
+    }
+
+    public void NextDialog()
+    {
+        dialogText.text = text[stage];
+        dialogImage.sprite = portraitSprites[stage];
+    }
+
+    protected IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        active = true;
+        yield return null;
     }
 }
--- Savor Saber/Assets/Scripts/Character/Dialog/DialogManualTrigger.cs
+++ Savor Saber/Assets/Scripts/Character/Dialog/DialogManualTrigger.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class DialogManualTrigger : MonoBehaviour
 {
+
+    public GameObject dialogTarget;
+    private Dialog dialog;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialog = dialogTarget.GetComponent<Dialog>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && Input.GetButtonDown("Cook"))
+        {
+            if(dialog != null)
+            {
+
+                dialog.Activate();
+            }
+            else
+            {
+                Debug.LogWarning("Warning: No dialog associated with trigger");
+            }
+        }
     }
 }
