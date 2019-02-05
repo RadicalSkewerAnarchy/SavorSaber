c355f413ee32f765978864f01d9da1dbd7faa31b
Lithiumantis <juyyek@ucsc.edu>
2019-02-05 15:43:33 -0800

    DialogData
    Created Dialog Data script

---
+++ Savor Saber/Assets/Scripts/Character/DialogData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains data to be used by conversation dialog boxes
+/// </summary>
+public class DialogData : MonoBehaviour
+{
+
+    public enum Emotions
+    {
+        Neutral,
+        Happy,
+        Sad,
+        Angry
+    }
+
+    public string displayName;
+
+    public Sprite portraitSpriteNeutral;
+
+
+}
