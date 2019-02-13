a1c9dc71c77902c4ff0748e06f243afa94ecaf4f
Lithiumantis <juyyek@ucsc.edu>
2019-02-13 14:16:32 -0800

    Main Menu
    - Basic main menu
    
    - Only "New Game" does anything, since we don't have options to change yet and Quit is ignored in the editor.
    
    - Known bugs: Buttons overlap at smaller resolutions, keybord input is funky

---
+++ Savor Saber/Assets/Scripts/Input/ButtonLycheeMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonLycheeMove : MonoBehaviour, IPointerEnterHandler
+{
+    public Image cursorSprite;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveLychee();
+    }
+
+    public void MoveLychee()
+    {
+        Debug.Log("selected");
+        cursorSprite.rectTransform.anchoredPosition = this.transform.localPosition - new Vector3(220, -16, 0);
+    }
+}
