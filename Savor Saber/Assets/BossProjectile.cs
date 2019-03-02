4c6b1a2e84687d1505b99ddad02c704a332d86df
magicalporpoise <pdstanle@ucsc.edu>
2019-03-02 01:02:05 -0800

    Fruitant Implementation
    incorporated more art from fruitants and began adding in new bois

---
+++ Savor Saber/Assets/BossProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossProjectile : BaseProjectile
+{
+    public GameObject mainProjectile;
+
+    enum FireMode
+    {
+        shotgun,
+        circle
+    }
+    FireMode firingMode = FireMode.shotgun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // randomly determine firing mode
+        // create more projectiles based on firing mode
+    }
+   
+}
