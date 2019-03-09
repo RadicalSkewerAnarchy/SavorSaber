3e41ac36190f4eb789716b0878a3d8c945d5dc47
Kindon <kindonsmith@gmail.com>
2019-03-08 16:08:03 -0800

    Setup secen and created basic files for pathfinding

---
+++ Savor Saber/Assets/Scripts/Utilities/CreateTilemapNodes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class CreateTilemapNodes : MonoBehaviour
+{
+    // Used for design in tilemap node population
+    // https://www.youtube.com/watch?v=htZFdfSLiYo
+    //
+
+    public Grid grid;
+    public Tilemap floor;
+    public List<Tilemap> obstacleLayers;
+    public GameObject nodePrefab;
+}
+
