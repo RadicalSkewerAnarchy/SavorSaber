b0562698361f2038e640676086037c6008ed328d
Lithiumantis <juyyek@ucsc.edu>
2019-03-10 17:16:19 -0700

    Marsh Entry Plant
    - Created marsh entry plant that closes up and prevents passage when Soma is nearby, unless fed a spicy thing
    
    - Uses new FlavorRecievers collision layer to handle attack collisions (as it has three different colliders that all need to do different things). This layer collides with attacks and nothing else.

---
+++ Savor Saber/Assets/Scripts/Character/FlavorInputManagers/MarshPlantFlavorInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarshPlantFlavorInput : PlantFlavorInput
+{
+    BoxCollider2D boxCollider;
+    CircleCollider2D recieverCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeDictionary();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        characterData = GetComponent<CharacterData>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponentInChildren<BoxCollider2D>();
+        recieverCollider = GetComponent<CircleCollider2D>();
+    }
+
+    public override void RespondToIngredients()
+    {
+        //handle spicy
+        if (flavorCountDictionary[RecipeData.Flavors.Spicy] >= 0)
+        {
+            isFed = true;
+            OpenPlant();
+        }
+    }
+
+    private void Update()
+    {
+    }
+
+    public override void ClosePlant()
+    {
+        boxCollider.enabled = true;
+        spriteRenderer.color = Color.red;
+    }
+    public override void OpenPlant()
+    {
+        boxCollider.enabled = false;
+        spriteRenderer.color = Color.white;
+    }
+}
---
+++ Savor Saber/Assets/Scripts/Character/FlavorInputManagers/PlantFlavorInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlantFlavorInput : FlavorInputManager
+{
+    [System.NonSerialized]
+    public bool isFed = false;
+
+    [System.NonSerialized]
+    public bool nearPlayer = false;
+
+    [System.NonSerialized]
+    public bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeDictionary();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        characterData = GetComponent<CharacterData>();
+    }
+
+    public override void RespondToIngredients()
+    {
+        //handle spicy
+        if (flavorCountDictionary[RecipeData.Flavors.Spicy] >= 0)
+        {
+            isFed = true;
+        }
+
+    }
+
+    public virtual void ClosePlant() { }
+    public virtual void OpenPlant() { }
+}
