758f5b94fa5bbeb586f3ec76fcaf5ba6564cccfa
Kindon <kindonsmith@gmail.com>
2019-03-11 15:38:17 -0700

    Beginnings of A*

---
+++ Savor Saber/Assets/Scripts/ArtificialIntelligence/Utility/Pathfinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<TileNode, float> cost = null;
+    TileNode[] discovered;
+    TileNode[] evaluated;
+
+    public void Start()
+    {
+        cost = new Dictionary<TileNode, float>();
+    }
+    /// <summary>
+    /// take in two tilenodes, the current and the target tile
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="target"></param>
+    public void AStar(TileNode current, TileNode target)
+    {
+        TileNode parent = null;
+
+
+
+
+    }
+}
