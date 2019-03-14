fbc155e514cfbe571a5d46f8099528faf2208202
kindon <kindonsmith@gmail.com>
2019-03-14 14:43:59 -0700

    Another big update to pathfinding
    -Added to TileNode.cs in order to keep track of neighboring tiles for pathfinding
    -Overhauled GraphNodePopulator.cs to calculate tiles based on collision layer and determine a node's neighbors adequately
    -Base implementation of A* algorithm in Pathfinder.cs, isn't currently tested or hooked up to anything
    -A* exists in the project, node based tilemaps with proper collisions and node tracking exist in the project, the only thing left is to join the two

--- Savor Saber/Assets/Scripts/ArtificialIntelligence/Utility/Pathfinder.cs
+++ Savor Saber/Assets/Scripts/ArtificialIntelligence/Utility/Pathfinder.cs
@@ -4,26 +4,88 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    Dictionary<TileNode, float> cost = null;
-    TileNode[] discovered;
-    TileNode[] evaluated;
+    Dictionary<TileNode, float> gScore;
+    Dictionary<TileNode, float> fScore;
+    public GameObject[] allNodes; 
 
     public void Start()
     {
-        cost = new Dictionary<TileNode, float>();
+        InitGraph();
     }
-    /// <summary>
-    /// take in two tilenodes, the current and the target tile
-    /// </summary>
-    /// <param name="current"></param>
-    /// <param name="target"></param>
-    public void AStar(TileNode current, TileNode target)
+    private void InitGraph()
     {
-        
+        gScore = new Dictionary<TileNode, float>();
+        fScore = new Dictionary<TileNode, float>();
+        var nodeList = GameObject.Find("Walkable");
+        for(int i = 0; i < nodeList.transform.childCount; i++)
+        {
+            gScore.Add(nodeList.transform.GetChild(i).GetComponent<TileNode>(), Mathf.Infinity);
+            fScore.Add(nodeList.transform.GetChild(i).GetComponent<TileNode>(), Mathf.Infinity);
+        }
+    }
 
+    /// <summary>
+    /// take in two tilenodes, the start and the target tile
+    /// GSCORE and FSCORE should contain graphs initialized based on populated grid
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="target"></param>
+    public TileNode[] AStar(TileNode start, TileNode target)
+    {
+        // set of evaluated nodes
+        List<TileNode> closed = new List<TileNode>();
+        // set of discovered but unevaluated nodes
+        List<TileNode> open = new List<TileNode>();
+        // the most efficient previous node mapped
+        Dictionary<TileNode, TileNode> cameFrom = new Dictionary<TileNode, TileNode>();
+        // the tilenode with its cost from the start node
+        gScore[start] = 0;
+        // total cost of getting from start to goal through this node, partly known, partly heuristic
+        fScore[start] = Vector3.Distance(start.transform.position, target.transform.position);
 
+        while (open.Count > 0)
+        {
+            TileNode current = null;
+            float min = 0;
+            foreach(var node in open)
+            {
+                if(fScore[node] < min)
+                {
+                    min = fScore[node];
+                    current = node;
+                }
+            }
+            if(current.GetInstanceID() == target.GetInstanceID())
+            {
+                //GOAL IS REACHED, RETURN PATH
+                break;
+            }
+            open.Remove(current);
+            closed.Add(current);
 
+            foreach(var neighbor in current.neighbors)
+            {
+                if (closed.Contains(neighbor))
+                {
+                    continue;
+                }
+                float tempScore = gScore[current] + Vector3.Distance(current.transform.position, neighbor.transform.position);
+                if (!open.Contains(neighbor))
+                {
+                    open.Add(neighbor);
+                }
+                else if(tempScore >= gScore[neighbor])
+                {
+                    continue;
+                }
+                cameFrom.Add(neighbor, current);
+                gScore[neighbor] = tempScore;
+                fScore[neighbor] = gScore[neighbor]; // + heuristic cost estimate
 
+            }
+        }
+
+        return null;
     }
 }
 //http://gigi.nullneuron.net/gigilabs/a-pathfinding-example-in-c/
--- Savor Saber/Assets/Scripts/ArtificialIntelligence/Utility/TileNode.cs
+++ Savor Saber/Assets/Scripts/ArtificialIntelligence/Utility/TileNode.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode]
 public class TileNode : MonoBehaviour
 {
-    public int x = 0, y = 0;
-    public bool isCollision = false;
+    public float x = 0, y = 0;
+    public bool walkable = true;
+    public List<TileNode> neighbors;
+    private void Awake()
+    {
+        neighbors = new List<TileNode>();
+    }
 }
