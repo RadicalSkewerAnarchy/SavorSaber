c62318f5b210a0aab126e359fff68a43fb09dade
Valentino Abate <valentinoabate1@gmail.com>
2019-04-04 20:59:51 -0700

    Ported code on to actual branch, blocked out chara select

---
+++ Savor Saber/Assets/Scripts/Input/PlayerTwoInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerTwoInput : MonoBehaviour
+{
+    public GameObject pressStartToJoin;
+    public GameObject characterSelect;
+    bool playerTwoActive = false;
+    // Update is called once per frame
+    void Update()
+    {
+        var names = Input.GetJoystickNames();
+        if (names.Where((s) => !string.IsNullOrWhiteSpace(s)).Count() < 2)
+            return;
+        if(playerTwoActive)
+        {
+
+        }
+        else 
+        {
+            pressStartToJoin.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Joystick2Button7))
+            {
+                characterSelect.SetActive(true);
+                playerTwoActive = true;
+                pressStartToJoin.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Savor Saber/Assets/Scripts/Player2/Player2Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="palyer2Data", menuName ="Player2 Data")]
+public class Player2Data : ScriptableObject
+{
+    public string displayName;
+    public Sprite charaSelectImage;
+    public GameObject playerPrefab;
+}
