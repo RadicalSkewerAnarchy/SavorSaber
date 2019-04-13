8d110453e75d0a8efc95bfa864b95f8c92db749d
magicalporpoise <pdstanle@ucsc.edu>
2019-04-13 14:31:20 -0700

    Fruitants Speak
    when pressing space bar near a fruitant, it tells the player about one of their favorite ingredients
    
    still need to link it to the controller input axis for "interact"

---
+++ Savor Saber/Assets/FavoriteFoodBubble.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FavoriteFoodBubble : MonoBehaviour
+{
+    public GameObject fruitant;
+    public GameObject fruit;
+    public SpriteRenderer bubbleRender;
+    public SpriteRenderer fruitRender;
+    public FlavorInputManager flavors;
+    public bool show = false;
+    public bool reset = true;
+    public Sprite favoriteFood;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flavors = fruitant.GetComponent<FlavorInputManager>();
+        bubbleRender = GetComponent<SpriteRenderer>();
+        fruitRender = fruit.GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Show();
+        if (Input.GetKeyDown("space"))
+        {
+            if (Vector2.Distance(player.transform.position, this.transform.position) < 2)
+            {
+                show = true;
+            }
+        }
+    }
+
+    private void Show()
+    {
+        if (show)
+        {
+            if (reset)
+            {
+                // get random favorite
+                int len = flavors.favoriteIngredients.Length;
+                string ff = flavors.favoriteIngredients[Random.Range(0, len-1)];
+                IngredientData d = player.GetComponentInChildren<RecipeDatabase>().allIngredients[ff];
+
+                // set sprite
+                fruitRender.sprite = d.image;
+                StartCoroutine(EndAfterSeconds(2));
+                reset = false;
+            }
+
+            fruitRender.enabled = true;
+            bubbleRender.enabled = true;
+        }
+        else
+        {
+            fruitRender.enabled = false;
+            bubbleRender.enabled = false;
+            reset = true;
+        }
+    }
+
+    protected IEnumerator EndAfterSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+        show = false;
+        yield return null;
+    }
+}
