a0ee4eddb2939527b8791a5edcf67b806a8abf79
Lithiumantis <juyyek@ucsc.edu>
2019-04-17 10:04:11 -0700

    Navigational aids
    - Added an "Info Display" object that will show an image on screen when the player reads it (spacebar)
    
    - Used this to make a prefab for showing the map. Can also manually define "you are here" arrow location.
    
    - Changed Amrita's dialogue so that she tells Soma to read the map if they're lost

---
+++ Savor Saber/Assets/Scripts/UI/InfoDisplay.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoDisplay : MonoBehaviour
+{
+    public bool isMap = true;
+    public Canvas UICanvas;
+
+    /// <summary>
+    /// A UI Image showing whatever info you want to appear on-screen
+    /// </summary>
+    public GameObject infoToDisplay;
+    public Vector2 youAreHereCoordinates;
+
+    private bool playerInRange = false;
+    private bool showingInfo = false;
+
+    private GameObject infoBox;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (InputManager.GetButtonDown(Control.Interact)&& playerInRange && !showingInfo)
+        {
+            infoBox = Instantiate(infoToDisplay, new Vector3(0, 0, 0), Quaternion.identity);
+            infoBox.transform.SetParent(UICanvas.transform);
+            infoBox.transform.localPosition = new Vector3(0, 0, 0);
+            infoBox.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+
+            //only use navigation arrows if this is displaying a map
+            if (isMap)
+            {
+                Transform arrow = infoBox.transform.GetChild(0);
+                if(arrow != null)
+                    arrow.localPosition = youAreHereCoordinates;
+            }
+
+
+            showingInfo = true;
+        }
+        else if (showingInfo && (InputManager.GetButtonDown(Control.Interact) || InputManager.GetButtonDown(Control.Cancel)))
+        {
+            Destroy(infoBox.gameObject);
+            showingInfo = false;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+
+}
