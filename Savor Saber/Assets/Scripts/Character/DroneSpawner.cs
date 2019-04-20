ecc64a8fac5df2af72bd77c4e1e473bc27eba30c
Lithiumantis <juyyek@ucsc.edu>
2019-04-20 16:25:25 -0700

    Drone Spawner
    - Spawns drones up to a maximum amount.
    
    - Can define amount of time to wait before checking if it can spawn a new drone
    
    - Can't be blocked yet

---
+++ Savor Saber/Assets/Scripts/Character/DroneSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DroneSpawner : MonoBehaviour
+{
+    public GameObject droneType;
+    public GameObject allNodes;
+
+    [SerializeField]
+    private int maxDrones = 3;
+    [SerializeField]
+    private int spawnInterval = 7;
+    private GameObject[] droneArray;
+    private WaitForSeconds intervalDelay;
+
+    private AudioSource spawnAudio;
+    private Pathfinder pf;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        droneArray = new GameObject[maxDrones];
+        intervalDelay = new WaitForSeconds(spawnInterval);
+        spawnAudio = GetComponent<AudioSource>();
+        StartCoroutine(SpawnLoop());
+    }
+
+    void SpawnDrones()
+    {
+        for(int i = 0; i < maxDrones; i++)
+        {
+            if(droneArray[i] == null)
+            {
+                //if you find an empty slot, spawn a new drone, then wait before checking again
+                droneArray[i] = Instantiate(droneType, transform.position, Quaternion.identity);
+                pf = droneArray[i].GetComponent<Pathfinder>();
+                if(allNodes != null)
+                    pf.allNodes = allNodes;
+
+                spawnAudio.Play();
+                StopCoroutine(SpawnLoop());
+                StartCoroutine(SpawnLoop());
+                return;
+            }
+        }
+        //if you found no empty slots, wait before checking again
+        StopCoroutine(SpawnLoop());
+        StartCoroutine(SpawnLoop());
+
+    }
+
+
+    private IEnumerator SpawnLoop()
+    {
+        yield return intervalDelay;
+        SpawnDrones();
+        yield return null;
+    }
+
+
+
+}
