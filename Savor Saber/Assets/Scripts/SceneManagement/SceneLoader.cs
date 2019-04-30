0f51182781ce3cb22952912f1d0b332ed04b1633
Valentino Abate <valentinoabate1@gmail.com>
2019-04-29 20:07:07 -0700

    Started scene loading code

---
+++ Savor Saber/Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    string[] scenes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sceneList = new List<Scene>();
+        //foreach(var name in scenes)
+        //    sceneList.Add(SceneManager.LoadScene(name))
+    }
+}
