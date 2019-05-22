c21347fed5cc562d5291292fb4d207f3dc590bb7
Lithiumantis <juyyek@ucsc.edu>
2019-05-21 18:34:20 -0700

    Laser Targeting System
    Three different variations on this system for testing:
    
    - Toggle on/off
    - Hold
    - Automatic activation when main skewer not empty
    
    (laser visuals are placeholder)

---
+++ Savor Saber/Assets/Scripts/Player/Targeting.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeting : MonoBehaviour
+{
+
+    private EntityController controller;
+    private Inventory inventory;
+    private SpriteRenderer sr;
+
+    private bool active;
+
+    [SerializeField]
+    private bool toggleActivate = true;
+    [SerializeField]
+    private bool holdActivate = false; //this sucks, don't use it
+    [SerializeField]
+    private bool activateOnSkewer = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInParent<EntityController>();
+        inventory = GetComponentInParent<Inventory>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetRotation();
+        SetActivation();
+    }
+
+    /// <summary>
+    /// Set the rotation of the laser pointer according to player controller
+    /// </summary>
+    void SetRotation()
+    {
+        if (controller.Direction == Direction.West)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        else if (controller.Direction == Direction.East)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+        else if (controller.Direction == Direction.North)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+        }
+        else if (controller.Direction == Direction.South)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        else if (controller.Direction == Direction.NorthWest)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, -45));
+        }
+        else if (controller.Direction == Direction.NorthEast)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, -135));
+        }
+        else if (controller.Direction == Direction.SouthWest)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+        }
+        else if (controller.Direction == Direction.SouthEast)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 135));
+        }
+    }
+
+    /// <summary>
+    /// Check for and execute selected activation methods
+    /// </summary>
+    void SetActivation()
+    {
+        //Note: change to use InputManager once a proper control scheme is decided
+        if (toggleActivate)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ToggleActive();
+            }
+        }
+        else if (holdActivate)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                active = true;
+                sr.enabled = true;
+            }
+            else
+            {
+                active = false;
+                sr.enabled = false;
+            }
+        }
+        else if (activateOnSkewer)
+        {
+            if (!inventory.ActiveSkewerEmpty())
+            {
+                active = true;
+                sr.enabled = true;
+            }
+            else
+            {
+                active = false;
+                sr.enabled = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Toggle laser on and off if toggle activation is selected
+    /// </summary>
+    void ToggleActive()
+    {
+        if (active)
+        {
+            active = false;
+            sr.enabled = false;
+        }
+        else
+        {
+            active = true;
+            sr.enabled = true;
+        }
+    }
+}
