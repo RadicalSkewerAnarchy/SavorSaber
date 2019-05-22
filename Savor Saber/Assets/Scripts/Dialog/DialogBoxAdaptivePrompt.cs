bb3e4610c4253df714c9239a5cd85b8d1a038d09
Lithiumantis <juyyek@ucsc.edu>
2019-05-22 03:57:47 -0700

    Dialog box fix
    - Dialog box detects if a controller is plugged in and changes the prompt accordingly
    
    - (haven't been able to test yet, don't own a controller. But the keyboard part works)
    
    - I don't think Tino did this yet as part of her adaptive controller support; I just noticed this issue during Tad's test and had a late night realization that it's really easy to fix, so I fixed it. If it conflicts I can just remove it.

---
+++ Savor Saber/Assets/Scripts/Dialog/DialogBoxAdaptivePrompt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DialogBoxAdaptivePrompt : MonoBehaviour
+{
+
+    private Text prompt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prompt = GetComponent<Text>();
+        string[] names = Input.GetJoystickNames();
+
+        if(names.Length == 0)
+        {
+            prompt.text = "(Space) ->";
+            Debug.Log("Dialog box detects keyboard");
+        }
+        else
+        {
+            prompt.text = "(Y) ->";
+            Debug.Log("Dialog box detects xbox controller");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
