d3082c511d943010862aa14abae4aaa0c08c9002
Kindon Smith <kimasmit@ucsc.edu>
2019-05-27 18:03:19 -0700

    Massive tilenode improvement, no longer lags on desert/marsh maps
    -Fixed large map tilenode performance issues for pathfinding
    -FPS increased by approximately 150% on plains, 300-500% on desert/marsh
    -No longer need to worry about map size for pathfinding to cause major issues.
    -Absurdly large maps would theoretically still cause lag

--- Savor Saber/Assets/Scripts/Utilities/GraphNodePopulator.cs
+++ Savor Saber/Assets/Scripts/Utilities/GraphNodePopulator.cs
@@ -9,7 +9,7 @@ public class GraphNodePopulator : MonoBehaviour
     public GameObject nodePrefab;
     public Tile[] discoveredTiles;
     public List<List<TileNode>> tiles;
-	public TileNode [][] tilesArr;
+	public TileNode [,] tilesArr = new TileNode[1000,1000];
     Tilemap[] activeTileMaps;
     BoundsInt bounds;
     bool walkable;
@@ -18,6 +18,7 @@ public class GraphNodePopulator : MonoBehaviour
     {
         tiles = new List<List<TileNode>>();
         tiles.Add(new List<TileNode>());
+        //tilesArr = new TileNode[,][];
         Populate();
     }
     private void Populate()
@@ -26,19 +27,22 @@ public class GraphNodePopulator : MonoBehaviour
         activeTileMaps = GetComponentsInChildren<Tilemap>();
         List<Tilemap> inactiveTileMaps = new List<Tilemap>();
         var graph = GraphSingleton.Instance;
+        var localGrass = new Tilemap();
         foreach(var active in activeTileMaps)
         {
             if(active.GetComponent<TilemapCollider2D>() != null)
             {
                 inactiveTileMaps.Add(active);
             }
-        }
+            if(active.name == "Grass" || active.name == "Ground") localGrass = active;
+        }   
         var activeTiles = inactiveTileMaps[0];
         //var inactiveTiles = inactiveTileMaps[1];
         //Debug.Log("Inactive and active tiles found");
         /// iterates through tilemap based on bounds
         int i = 0, j = 0;
         bounds = activeTiles.cellBounds;
+        //tilesArr[i] = new TileNode[1000];
         for (int x = bounds.xMin; x < bounds.xMax; x++)
         {
             j = 0;
@@ -58,7 +62,7 @@ public class GraphNodePopulator : MonoBehaviour
                 {
                     walkable = false;
                 }
-                if((y % clusterLimit == 0) && (x % clusterLimit == 0) && walkable)
+                if((y % clusterLimit == 0) && (x % clusterLimit == 0) && walkable && localGrass.HasTile(local))
                 {
                     GameObject tile = Instantiate(nodePrefab, current + new Vector3(.25f,.25f), new Quaternion(0, 0, 0, 1));
                     tile.transform.SetParent(parent.transform);
@@ -68,26 +72,26 @@ public class GraphNodePopulator : MonoBehaviour
                     tile.GetComponent<TileNode>().SetWalkable(walkable);
 					tile.GetComponent<TileNode>().active = true;
                     tiles[i].Add(tile.GetComponent<TileNode>());
-                    //tilesArr[i][j] = tile.GetComponent<TileNode>();
+                    tilesArr[i, j] = tile.GetComponent<TileNode>();
+                    //Debug.Log("TilesArr[i][j] : " + tilesArr[i, j]);
                     graph.tiles.Add(tile);
-					j+=clusterLimit;
-                }else{
-					var tempTile = Instantiate(nodePrefab, current, new Quaternion(0,0,0,1), activeTiles.transform.GetChild(1));
-                    tempTile.GetComponent<TileNode>().SetWalkable(false);
-					tiles[i].Add(tempTile.GetComponent<TileNode>());
-					j+=clusterLimit;
-				}
+                }
+                j++;
             }
             i++;
         }
 
-        for (int x = 0; x < tiles.Count - clusterLimit; x++)
+        for (int x = 0; x < tilesArr.GetLength(0) - clusterLimit; x++)
         {
-
-            for (int y = 0; y < tiles[x].Count - clusterLimit; y++)
+            if(tilesArr.GetLength(x) <= clusterLimit ) continue;
+            for (int y = 0; y < tilesArr.GetLength(x) - clusterLimit; y++)
             {
-				if(!tiles[x][y].active) continue;
-                var node = tiles[x][y];
+                if(tilesArr[x,y] == null) continue;
+
+
+				//if(!tiles[x][y].active) continue;
+                //var node = tiles[x][y];
+                var node = tilesArr[x,y];
                 for (int m = -clusterLimit; m <= clusterLimit; m+= clusterLimit)
                 {
                     for (int n = -clusterLimit; n <= clusterLimit; n+= clusterLimit)
@@ -96,7 +100,9 @@ public class GraphNodePopulator : MonoBehaviour
 						if(m == 0 && n == 0) continue;
                         try
                         {
-							if(tiles[x+m][y+n].active)	node.neighbors.Add(tiles[x+m][y+n]);
+
+							//if(tiles[x+m][y+n].active)	node.neighbors.Add(tiles[x+m][y+n]);
+                            if(tilesArr[x+m,y+n].active) node.neighbors.Add(tilesArr[x+m, y+n]);
 
                         //node.neighbors.Add(tiles[x + m][y + n]);
 						//if((tiles[x+m][y+n].x != 0  ) && ( tiles[x+m][y+n].y != 0)){
