da7da1a5631819867e586c5fb8918c422da1d957
Valentino Abate <valentinoabate1@gmail.com>
2019-05-30 00:12:00 -0700

    Party hats restored
    -Pride hat enabled on pride month

---
+++ Savor Saber/Assets/Scripts/Misc/PartyHatSwapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PartyHatSwapper : MonoBehaviour
+{
+    public Sprite gayhat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (System.DateTime.Now.Month == 6)
+            GetComponent<SpriteRenderer>().sprite = gayhat;
+    }
+}
