829cc4272dcf9f5da5952fef92ee59d89f708250
Lithiumantis <juyyek@ucsc.edu>
2019-05-30 21:01:33 -0700

    Drone Nerfs
    -Applied drone projectile nerfs to the actual prefab
    
    - Killing all drones from a spawner resets that spawner's timer
    
    - Moved northern desert camp so you don't spawn next to a drone spawner
    
    - Adjusted Marsh Poison Plant POI
    
    - reflected projectiles can damage drones (and speed up after reflected)

--- Savor Saber/Assets/Scripts/Player/PlayerKnifeAttack.cs
+++ Savor Saber/Assets/Scripts/Player/PlayerKnifeAttack.cs
@@ -64,6 +64,12 @@ public class PlayerKnifeAttack : BaseMeleeAttack
             {
                 body.directionVector *= -1;
                 Physics2D.IgnoreCollision(body.GetComponent<Collider2D>(), body.attacker.GetComponent<Collider2D>(), false);
+                if(!body.hurtDrones)
+                    body.hurtDrones = true;
+                if (body.attacker != null)
+                    body.attacker = this.gameObject;
+
+                body.projectileSpeed += 3;
             }              
         }
     }
