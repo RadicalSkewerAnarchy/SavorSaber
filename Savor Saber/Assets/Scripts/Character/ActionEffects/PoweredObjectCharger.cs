ab4be51d91f39a3e87a3cd3704cc5511bfe68b57
kindon <kindonsmith@gmail.com>
2019-07-25 16:03:32 -0700

    PricklePearUpdate!
    -PricklePear is now CHONKY:
    // Increased PricklePear Health
    // Reduced PricklePear Speed
    // Increased PricklePear's Mass and Drag slightly, may have to revert if large numbers of PricklePears cause player to get stuck...
    
    -PricklePear constantly generates an electric field that damages enemies but not very quickly
    
    -When overcharged, electric AoE effect changes color(programmer art), does more damage per tic, and charges turns on chargeable objects

--- Savor Saber/Assets/Scripts/Character/ActionEffects/PoweredObjectCharger.cs
+++ Savor Saber/Assets/Scripts/Character/ActionEffects/PoweredObjectCharger.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PoweredObjectCharger : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int damageBoostValue = 4;
     void Start()
     {
         
@@ -14,13 +15,13 @@ public class PoweredObjectCharger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<SpriteRenderer>().color = new Color(0,244,255,255);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PoweredObject targetObject = collision.gameObject.GetComponent<PoweredObject>();
-        if (targetObject == null || !targetObject.canBeSourCharged)
+        if (targetObject == null || !targetObject.canBeSourCharged || enabled == false)
             return;
 
         targetObject.TurnOn();
