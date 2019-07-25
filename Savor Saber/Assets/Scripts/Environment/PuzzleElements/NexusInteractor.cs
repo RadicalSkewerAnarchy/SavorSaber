a5db59d16c35fc8d3168939682ea36f7bd0c5d50
Valentino Abate <valentinoabate1@gmail.com>
2019-07-24 17:01:03 -0700

    Nexus 1
    -Interactable nexus that will spawn. ingredients
    -Does not restore ingredients to an area yet (will want to rebase destructible environment first)
    - Refactored and cleaned up Spawner code (now AreaSpawner and LoopedSpawner
    - Cleaned up Destructible environment code in preparation for rebase
    -Fully deprecated and removed AIGroup
    - NEW COLLISION LAYER (NoCollision) for a cleaner layer for theings (like spawn areas) that don't actually ever collide with anything

---
+++ Savor Saber/Assets/Scripts/Environment/PuzzleElements/NexusInteractor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class NexusInteractor : MonoBehaviour
+{
+    public AreaSpawner spawner;
+    public float spawnCooldownTime = 2.5f;
+
+    // Interact to spawn fields
+    private Collider2D interactArea;
+    private bool playerInInteractArea;
+    private bool readyToSpawn;
+
+    private void Awake()
+    {
+        interactArea = GetComponent<Collider2D>();
+        readyToSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(readyToSpawn && playerInInteractArea && InputManager.GetButtonDown(Control.Interact))
+        {
+            spawner.Spawn();
+            readyToSpawn = false;
+            StartCoroutine(SpawnCooldownCr());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerInInteractArea = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInInteractArea = false;
+    }
+
+    private IEnumerator SpawnCooldownCr()
+    {
+        yield return new WaitForSeconds(spawnCooldownTime);
+        readyToSpawn = true;
+    }
+}
---
+++ Savor Saber/Assets/Scripts/Environment/Spawners/AreaSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A component that spawns objects within an area.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class AreaSpawner : MonoBehaviour
+{    
+    [SerializeField] protected GameObject[] spawnObjects = new GameObject[1];
+    [SerializeField] protected GameObject[] spawnSignals = new GameObject[0];
+    [SerializeField] protected List<GameObject> trackedObjects = new List<GameObject>();
+
+    public int maxSpawnedEntites= 8;
+    public int maxTries = 100;
+    public bool spawnAttatched = false;
+
+    new protected Collider2D collider;
+    
+    private void Start()
+    {
+        RemoveDeadObjects();
+        collider = GetComponent<Collider2D>();
+    }
+
+    public virtual void Spawn()
+    {
+        RemoveDeadObjects();
+        bool objectSpawned = false;
+        // Attempt to spawn each object in spawn objects
+        foreach(var go in spawnObjects)
+        {
+            if (trackedObjects.Count >= maxSpawnedEntites)
+                break;
+            for (int i = 0; i < maxTries; ++i)
+            {
+                var bounds = collider.bounds;
+                Vector2 spawnPos = new Vector2(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y));
+                if (collider.OverlapPoint(spawnPos) && Physics2D.OverlapPoint(spawnPos, 0) == null)
+                {
+                    var newObj = Instantiate(go, spawnPos, Quaternion.identity);
+                    objectSpawned = true;
+                    trackedObjects.Add(newObj);
+
+                    // if it's a skewerable object,
+                    // set momentum to 0
+                    if (newObj.tag == "SkewerableObject" && spawnAttatched)
+                    {
+                        newObj.GetComponent<SkewerableObject>().Attached = true;
+                    }
+                    break;
+                }
+            }
+        }
+        // If at least one object was spawned, spawn all signals
+        if(objectSpawned)
+        {
+            foreach(var go in spawnSignals)
+            {
+                var newObj = Instantiate(go, transform);
+            }
+        }
+    }
+
+    public void RemoveDeadObjects()
+    {
+        trackedObjects.RemoveAll((obj) => obj == null);
+    }
+
+    public GameObject[] GetMembers()
+    {
+        RemoveDeadObjects();
+        return trackedObjects.ToArray();
+    }
+}
