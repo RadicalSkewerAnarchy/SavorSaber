01ba4b11d9aefc7ee98778f53e046a54cbce3f7c
Lithiumantis <juyyek@ucsc.edu>
2019-07-25 20:25:49 -0700

    Armor and AIGroup
    - Added "armored" flag to character data. If set, entity can only be damaged by overcharged attacks
    
    - Changed DoDamage() in character data to have an optional "overcharged" parameter to identify overcharged attacks
    
    - Restored AIGroup.cs file to fix compile errors

---
+++ Savor Saber/Assets/Scripts/Environment/Spawners/AIGroup.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGroup : MonoBehaviour
+{
+    public List<GameObject> Members
+    {
+        get
+        {
+            RemoveDeadObjects();
+            return members;
+        }
+    }
+    [SerializeField] private List<GameObject> members = new List<GameObject>();
+
+    public void AddMember(GameObject member)
+    {
+        members.Add(member);
+    }
+    public void KickMember(GameObject member)
+    {
+        members.Remove(member);
+    }
+    public bool IsMember(GameObject member)
+    {
+        return members.Find((obj) => obj == member) != null;
+    }
+    public void RemoveDeadObjects()
+    {
+        members.RemoveAll((obj) => obj == null);
+    }
+}
