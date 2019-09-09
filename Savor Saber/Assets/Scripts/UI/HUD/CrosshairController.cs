97fb203c76c0833591026954a763049abaac2311
Lithiumantis <juyyek@ucsc.edu>
2019-09-09 14:07:06 -0700

    Preliminary controller support for 360 targeting

---
+++ Savor Saber/Assets/Scripts/UI/HUD/CrosshairController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+    public float crosshairSpeed = 10;
+    private Vector2 controllerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.ControllerMode)
+        {
+            controllerInput.x = InputManager.GetAxis()
+        }
+        else
+        {
+            transform.position = GetMouseTarget();
+        }
+    }
+
+    private Vector2 GetMouseTarget()
+    {
+        Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return target;
+    }
+}
