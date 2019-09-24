01b94fee752797fdb2a94265299922c4014c79d7
Lithiumantis <juyyek@ucsc.edu>
2019-09-23 19:40:43 -0700

    New Area Text
    Added NewAreaTextTrigger prefab to prefabs/UI. To use it, drag the prefab into the scene and change its color and text fields as desired.
    
    Displays animated text on screen when entering a new area
    
    Can be set to be repeatable. If repeatable, cannot be triggered again until the animated text prefab sends a signal to the trigger that it's done animating.

---
+++ Savor Saber/Assets/Scripts/UI/NewAreaText.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewAreaText : MonoBehaviour
+{
+
+    public GameObject animTextTemplate;
+    public GameObject typingTextTemplate;
+    public GameObject targetCanvas;
+    public string animatedTextTop;
+    public string animatedTextBottom;
+    public bool animated = true; //whether to use the animated or typing variant
+    public bool repeatable = false;
+    public Vector3 color;
+    [System.NonSerialized]
+    public bool textPlaying = false;
+    private bool active = true;
+    private GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hide the SpriteRenderer used for in-editor visualization
+        GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("text playing? " + textPlaying);
+        if (targetCanvas == null)
+            targetCanvas = GameObject.Find("DialogCanvas");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(active && !textPlaying && collision.gameObject.tag == "Player")
+        {
+            if (animated)
+            {
+                text = Instantiate(animTextTemplate, targetCanvas.transform);
+                NewAreaTextAnimShutoff shutoff = text.GetComponentInChildren<NewAreaTextAnimShutoff>();
+                shutoff.textTrigger = this;
+
+                //assign proper text and color
+                Text textTop = text.transform.GetChild(0).gameObject.GetComponent<Text>();
+                Text textBottom = text.transform.GetChild(1).gameObject.GetComponent<Text>();
+                textTop.text = animatedTextTop;
+                textBottom.text = animatedTextBottom;
+                textTop.color = new Color(color.x, color.y, color.z);
+                textBottom.color = new Color(color.x, color.y, color.z);
+                //play audio as well? 
+            }
+            else
+            {
+                //Instantiate the typing variant once that exists
+            }
+
+            textPlaying = true;
+            if (!repeatable)
+                active = false;
+
+        }
+    }
+}
---
+++ Savor Saber/Assets/Scripts/UI/NewAreaTextAnimShutoff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewAreaTextAnimShutoff : MonoBehaviour
+{
+    public NewAreaText textTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableTrigger()
+    {
+        Debug.Log("Called EnableTrigger successfully");
+        if(textTrigger != null)
+            textTrigger.textPlaying = false;
+
+        Destroy(transform.parent.gameObject);
+    }
+}
