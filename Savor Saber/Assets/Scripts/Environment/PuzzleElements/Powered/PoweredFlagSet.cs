1612e17a532367f5c46445fc5386b7b54cde6ca8
Lithiumantis <juyyek@ucsc.edu>
2019-10-25 19:27:04 -0700

    Combat intro cutscene
    - Hacking the terminal now properly sets up the next cutscene

---
+++ Savor Saber/Assets/Scripts/Environment/PuzzleElements/Powered/PoweredFlagSet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredFlagSet : PoweredObject
+{
+
+    public string[] flagsToSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TurnOn()
+    {
+        foreach(string flag in flagsToSet)
+        {
+            FlagManager.SetFlag(flag, "true");
+        }
+    }
+
+    public override void ShutOff()
+    {
+        foreach(string flag in flagsToSet)
+        {
+            FlagManager.SetFlag(flag, "false");
+        }
+    }
+}
