a10c041c6257e790b7f548176bea0fa4ee5d961b
Lithiumantis <juyyek@ucsc.edu>
2019-11-06 13:02:26 -0800

    Maps and Gates
    - Ported revised tilemap over to official marsh scene. Removed most environment objects.
    
    - Created Logic Gate entities for puzzles. Prefabs include NOT, AND, OR, and XOR.
    
    - Only supports two inputs to a single gate, as I didn't expect we'd need more for how complex our puzzles will be, but they can be expanded if need be (or just use multiple gates).
    
    - To send an input to a gate, call the TurnOn() or ShutOff() functions of its child objects

---
+++ Savor Saber/Assets/Scripts/Environment/PuzzleElements/Powered/LogicMulti.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicMulti : MonoBehaviour
+{
+    public enum LogicType
+    {
+        AND,
+        OR,
+        XOR
+    }
+
+    public LogicType type = LogicType.AND;
+    [HideInInspector]
+    public bool[] inputs = new bool[2];
+    public PoweredObject[] targetObjects;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetInputValue(int input, bool value)
+    {
+        inputs[input] = value;
+
+        //Logic AND
+        if(type == LogicType.AND)
+        {
+            if(inputs[0] && inputs[1])
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.TurnOn();
+                }
+            }
+            else
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.ShutOff();
+                }
+            }
+        }
+        else if(type == LogicType.OR)
+        {
+            if (inputs[0] || inputs[1])
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.TurnOn();
+                }
+            }
+            else
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.ShutOff();
+                }
+            }
+        }
+        else if (type == LogicType.XOR)
+        {
+            if (inputs[0] ^ inputs[1])
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.TurnOn();
+                }
+            }
+            else
+            {
+                foreach (PoweredObject target in targetObjects)
+                {
+                    target.ShutOff();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Logic gate error: " + type + " is not a supported operation type.");
+        }
+    }
+}
---
+++ Savor Saber/Assets/Scripts/Environment/PuzzleElements/Powered/LogicMultiInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicMultiInput : PoweredObject
+{
+    private LogicMulti baseMulti;
+    public int inputIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseMulti = GetComponentInParent<LogicMulti>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TurnOn()
+    {
+        base.TurnOn();
+        baseMulti.SetInputValue(inputIndex, true);
+    }
+
+    public override void ShutOff()
+    {
+        base.ShutOff();
+        baseMulti.SetInputValue(inputIndex, false);
+    }
+}
