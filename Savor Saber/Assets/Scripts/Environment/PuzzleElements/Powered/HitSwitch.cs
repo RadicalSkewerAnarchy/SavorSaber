a21cffc7717db7d06b2900043479af91f90e23ca
Lithiumantis <juyyek@ucsc.edu>
2019-11-11 17:29:17 -0800

    stuff
    - Created new FlavorInputManager variant that can signal to powered objects. Used this for marsh puzzle 1.
    
    - More objects have shadows now

---
+++ Savor Saber/Assets/Scripts/Environment/PuzzleElements/Powered/HitSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class HitSwitch : FlavorInputManager
+{
+
+    public PoweredObject[] TargetObjects;
+    private bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Feed(IngredientData[] ingredientArray, bool fedByPlayer)
+    {
+        if (ingredientArray.Length >= 0)
+        {
+            if (active)
+            {
+                foreach (PoweredObject target in TargetObjects)
+                {
+                    target.ShutOff();
+                }
+                active = false;
+            }
+            else
+            {
+                foreach (PoweredObject target in TargetObjects)
+                {
+                    target.TurnOn();
+                }
+                active = true;
+            }
+
+        }
+    }
+
+}
