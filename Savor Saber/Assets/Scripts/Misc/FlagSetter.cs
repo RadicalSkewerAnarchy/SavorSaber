ae58cd0a6444003ad56e193f48511b316f76ab7b
Lithiumantis <juyyek@ucsc.edu>
2020-02-03 12:58:19 -0800

    scenes
    - Added logic branches to post-clover cutscene graph for various objective completion states

---
+++ Savor Saber/Assets/Scripts/Misc/FlagSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetFlagTrue(string flag)
+    {
+        FlagManager.SetFlag(flag, "True");
+    }
+
+    public void SetFlagFalse(string flag)
+    {
+        FlagManager.SetFlag(flag, "False");
+    }
+}
