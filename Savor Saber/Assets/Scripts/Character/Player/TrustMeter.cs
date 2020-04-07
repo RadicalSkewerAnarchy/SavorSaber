8a1e61e5192a61f11dbd4d9f6c53099724509ddb
Lithiumantis <juyyek@ucsc.edu>
2020-04-06 19:30:57 -0700

    Trust buff basic implementation
    - SetTrust() now sets a trust stage enum in TrustMeter.cs and calls SetTrustEffect(RecipeData.Flavors flavor) if the level changes.
    
    - SetTrustEffect applies one of several effects based on the flavor, with certain parameters scaling based on the trust level.
    
    - Added framework for player buffs to TrustMeter.cs
    
    - Added GetSpeed() and SetSpeed() method to PlayerController.cs. Would have just used a getter and setter but it seems like some unity attributes don't seem to work with those
    
    - Added oil field textures and a placeholder fire effect

--- Savor Saber/Assets/Scripts/Character/Player/TrustMeter.cs
+++ Savor Saber/Assets/Scripts/Character/Player/TrustMeter.cs
@@ -14,9 +14,27 @@ public class TrustMeter : MonoBehaviour
 
     private int maxTrust = 100;
     private int minTrust = 0;
+    
+    public enum TrustStage {Neutral, Warm, Allied, Honored}
+    private TrustStage stage;
+
+    private RecipeData.Flavors companionFlavor;
+
+    //component fields
+    private PlayerData somaData;
+    private PlayerController somaController;
+    private float baseSpeed;
+    private int baseMaxHealth;
+
     // Start is called before the first frame update
     void Start()
     {
+        somaData = GetComponent<PlayerData>();
+        somaController = GetComponent<PlayerController>();
+        baseSpeed = somaController.GetSpeed();
+        baseMaxHealth = somaData.maxHealth;
+
+
         meterFill = meterSlider.fillRect.gameObject.GetComponent<Image>();
         ChangeTrust(10);
     }
@@ -38,33 +56,75 @@ public class TrustMeter : MonoBehaviour
         else if (trust < 0)
             trust = 0;
 
+        //update HUD meter
         if(meterSlider != null)
         {
             meterSlider.value = trust;
 
             float colorvalue = 0.8f - ((float)trust / 100f) / 2;
             meterFill.color = new Color(1, colorvalue, 0.9f);
-
         }
-    }
 
-    public void SetTrust(int amount)
-    {
-        trust = amount;
-        if (trust > maxTrust)
-            trust = maxTrust;
-        else if (trust < minTrust)
-            trust = minTrust;
-
-        if (meterSlider != null)
+        //set trust level
+        TrustStage lastStage = stage;
+        if(trust < 25)
         {
-            meterSlider.value = trust;
+            stage = TrustStage.Neutral;
+        }
+        else if(trust < 50)
+        {
+            stage = TrustStage.Warm;
+        }
+        else if (trust < 75)
+        {
+            stage = TrustStage.Allied;
+        }
+        else if (trust < 100)
+        {
+            stage = TrustStage.Honored;
+        }
+        //if the stage has changed, reset the trust effect with the same flavor but new magnitude
+        if (stage != lastStage)
+            SetTrustEffect(companionFlavor);
+    }
 
-            float colorvalue = 0.8f - ((float)trust / 100) / 2;
-            meterFill.color = new Color(1, colorvalue, 0.9f);
+    public int GetTrust()
+    {
+        return trust;
+    }
 
+    public void SetTrustEffect(RecipeData.Flavors flavor)
+    {
+        ResetPlayerParameters();
+        switch (flavor)
+        {
+            //sweet companions buff health and speed
+            case RecipeData.Flavors.Sweet:
+                Debug.Log("Setting trust effect for sweet");
+                somaData.maxHealth = baseMaxHealth + (int)stage;
+                somaController.SetSpeed(baseSpeed + (30 * (int)stage));
+                break;
+            //spicy companions add DoT effects to skewer throws vs. enemies
+            case RecipeData.Flavors.Spicy:
+                Debug.Log("Setting trust effect for spicy");
+                break;
+            //salty companions generate a shield
+            case RecipeData.Flavors.Salty:
+                Debug.Log("Setting trust effect for salty");
+                break;
+            //sour companions generate a tesla field
+            case RecipeData.Flavors.Sour:
+                Debug.Log("Setting trust effect for sour");
+                break;
         }
     }
 
-
+    //resets player stats to their default values, unaffected by fruitant companion buffs
+    private void ResetPlayerParameters()
+    {
+        somaController.SetSpeed(baseSpeed);
+        somaData.maxHealth = baseMaxHealth;
+        if (somaData.health > baseMaxHealth)
+            somaData.health = baseMaxHealth;
+    }
 }
