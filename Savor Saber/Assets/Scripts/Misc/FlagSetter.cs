53197d1c62bc969593d4b760c98a83483660d9cb
Lithiumantis <juyyek@ucsc.edu>
2020-04-10 19:12:38 -0700

    scene save

--- Savor Saber/Assets/Scripts/Misc/FlagSetter.cs
+++ Savor Saber/Assets/Scripts/Misc/FlagSetter.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class FlagSetter : MonoBehaviour
 {
+    public bool resetFlagsOnLoad = false;
+
+    [Header("Flags to automatically set")]
+    public string[] flagsToBeTrue;
+    public string[] flagsToBeFalse;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        SetAutoFlags();
     }
+
+    private void SetAutoFlags()
+    {
+        foreach (string flag in flagsToBeTrue)
+        {
+            if (resetFlagsOnLoad)
+            {
+                SetFlagTrue(flag);
+            }
+            else if (!((FlagManager.GetFlag(flag) == "True") || (FlagManager.GetFlag(flag) == "False")))
+            {
+                SetFlagTrue(flag);
+            }
+
+        }
+
+        foreach (string flag in flagsToBeFalse)
+        {
+            if (resetFlagsOnLoad)
+            {
+                SetFlagFalse(flag);
+            }
+            else if (!((FlagManager.GetFlag(flag) == "True") || (FlagManager.GetFlag(flag) == "False")))
+            {
+                SetFlagFalse(flag);
+            }
+        }
+    }
+
 
     public void SetFlagTrue(string flag)
     {
