816c28891485574c4470fb62603076dd6a3e39ff
philipdrexler <pstanley@foresightsports.com>
2020-04-27 20:04:46 -0700

    AI Commandable
    - created base class for commandable state for any ai agent
    --- needs states
    --- needs link to ai commander

---
+++ Savor Saber/Assets/Scripts/Character/NPC/AI/_NewBrain/Extensions/States/AIStateCommandable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AIStateCommandable : AIState
+{
+    [SerializeField]
+    private List<AIData.Protocols> Verbs;
+    [SerializeField]
+    private List<AIState> Performances;
+
+    private int commandOfChoice = -1;
+
+
+    public override void Perform()
+    {
+        if (commandOfChoice >= 0)
+        {
+            AIState state = Performances[commandOfChoice];
+            if (state != null)
+            {
+                state.Perform();
+            }
+        }
+    }
+
+    public override void OnEnter()
+    {
+        foreach (var state in Performances)
+        {
+            state.SetBrain(this.myBrain);
+        }
+    }
+
+    public void SetCommand(AIData.Protocols verb)
+    {
+        if (Verbs.Contains(verb))
+        {
+            commandOfChoice = Verbs.IndexOf(verb);
+        }
+        else
+        {
+            commandOfChoice = -1;
+        }
+    }
+}
