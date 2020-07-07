39286597e29439cc4bfd17607e138a8b949ec8e6
Lithiumantis <juyyek@ucsc.edu>
2020-07-07 14:20:11 -0700

    Swamp03 Changes
    - Fixed pathfinding by allowing proper node generation in cave biome
    
    - Added Material Change Trigger script and prefab to handle lighting effects in cave biome
    
    - Increased speed of Ghost Pepper as test for new QoL changes
    
    - Salt Shields no longer collide with player, allowing the player to enter and exit at their leisure
    
    - Increased Drone Boss health in Swamp03
    
    - Increased Shield mechanic timer in Swamp03 to make timing less sensitive

---
+++ Savor Saber/Assets/Scripts/Lighting/MaterialChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class MaterialChanger : MonoBehaviour
+{
+
+    public Material targetMaterial;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SpriteRenderer targetSR = collision.gameObject.GetComponent<SpriteRenderer>();
+
+        if(targetSR != null)
+        {
+            targetSR.material = targetMaterial;
+        }
+    }
+
+}
