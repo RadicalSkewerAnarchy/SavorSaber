db578e9e1d772f9de45f12d076c3a5700d578118
Lithiumantis <juyyek@ucsc.edu>
2020-10-05 15:43:52 -0700

    No duplicate Somas
    - Fixed intro cutscene to hide active player GameObject until the fake damaged Soma has stood up and been removed

---
+++ Savor Saber/Assets/Scripts/EventsAndCutscenes/IntroSomaHide.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSomaHide : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject soma;
+    private SpriteRenderer somaRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetPlayer()
+    {
+        soma = GameObject.Find("Soma");
+        if (soma == null)
+        {
+            Debug.LogError("No instance of Soma detected");
+        }
+        somaRenderer = soma.GetComponent<SpriteRenderer>();
+    }
+
+    public void HidePlayer()
+    {
+        if(somaRenderer == null)
+            GetPlayer();
+        somaRenderer.enabled = false;
+    }
+
+    public void ShowPlayer()
+    {
+        if (somaRenderer == null)
+            GetPlayer();
+        somaRenderer.enabled = true;
+    }
+
+
+}
