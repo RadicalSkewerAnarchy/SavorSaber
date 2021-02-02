1d14c8f77624cee48fcd0bb1a2fa0921bd32c3ce
Lithiumantis <juyyek@ucsc.edu>
2021-02-01 18:09:28 -0800

    Panacea AI and Boss Sprites
    - Spread Shot bosses now have unique sprites
    
    - Implemented Panacea walking behavior (will move towards destination when player is within a certain radius, idle otherwise)
    
    - In order to make her more receptive to AI commands, Panacea has been replaced with a Paddle Pear wearing her sprite (or, really, Mana's sprite since Panacea's animations don't exist yet)
    
    - Nec is jealous

---
+++ Savor Saber/Assets/Scripts/Entities/Character/NPC/AI/Brain/Specifc Brains/PanaceaData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanaceaData : AIData
+{
+    public override Protocols DecideProtocol()
+    {
+        return Protocols.Lazy;
+    }
+}
---
+++ Savor Saber/Assets/Scripts/QuestEvents/PanaceaMover.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanaceaMover : PoweredObject
+{
+
+    private bool playerNearby = false;
+    [SerializeField]
+    private Commander AICommander;
+    [SerializeField]
+    private GameObject escortTarget;
+    private AIData escortData;
+
+    public GameObject waypoint;
+
+    private BasicCharacterController panaceaController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AICommander == null)
+            AICommander = GameObject.Find("Gaia").GetComponent<Commander>();
+
+        escortData = escortTarget.GetComponent<AIData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TurnOn()
+    {
+        base.TurnOn();
+        if (playerNearby)
+            StartMotion();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerNearby = true;
+            if(active)
+                StartMotion();
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerNearby = false;
+            if (active)
+                StopMotion();
+        }
+    }
+
+    private void StartMotion()
+    {
+        Debug.Log("Escort quest: Starting motion");
+        AICommander.Command(escortTarget, AIData.Protocols.Chase, waypoint, waypoint.transform.position);
+        escortData.Speed = 1;
+    }
+    private void StopMotion()
+    {
+        Debug.Log("Escort quest: Stopping motion");
+        AICommander.Command(escortTarget, AIData.Protocols.Lazy, waypoint, waypoint.transform.position);
+        escortData.Speed = 0;
+
+    }
+}
