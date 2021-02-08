f0fc0601a734c6fe1c1e266aea0b1900a3f16ed2
Lithiumantis <juyyek@ucsc.edu>
2021-02-08 03:15:07 -0800

    Spawners and Dialogue Boxes
    - Changed ContactEventTrigger prefab to be activated by a specified tag, rather than just "Player"
    
    - Created a spawner that can spawn entities from a trigger, rather than a repeating loop
    
    - Increased size of dialogue boxes and reduced font size slightly in order to accomodate more dialogue.

---
+++ Savor Saber/Assets/Scripts/Entities/Spawners/TriggeredSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spawnedObject;
+    private ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        Instantiate(spawnedObject, transform.position, Quaternion.identity);
+        particle.Play();
+    }
+}
--- Savor Saber/Assets/Scripts/EventsAndCutscenes/Triggers/GenericContactTrigger.cs
+++ Savor Saber/Assets/Scripts/EventsAndCutscenes/Triggers/GenericContactTrigger.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using UnityEngine.Events;
 public class GenericContactTrigger : PoweredObject
 {
-    public UnityEvent callOnActivation = new UnityEvent();
+    public string tagToCheck;
     public bool repeatable = false;
     private bool triggered = false;
+    public UnityEvent callOnActivation = new UnityEvent();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +24,7 @@ public class GenericContactTrigger : PoweredObject
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (active && collision.gameObject.tag == "Player")
+        if (active && collision.gameObject.tag == tagToCheck)
         {
             if(repeatable || !triggered)
             {
