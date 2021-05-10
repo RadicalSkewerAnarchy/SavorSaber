360680ef9d6651270e983dceaf43652a49e5451a
Lithiumantis <juyyek@ucsc.edu>
2021-05-10 16:50:59 -0700

    Lab Cutscenes and scenery
    - Added some cutscenes to Desert03
    
    - Added new signage props
    
    - Added ramp tiles to lab set

---
+++ Savor Saber/Assets/Scripts/EventsAndCutscenes/Dialog/DroneTransmissionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DroneTransmissionHelper : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    bool alive = true;
+    public GameObject parentDrone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(alive)
+            transform.position = parentDrone.transform.position;
+    }
+
+    public void SpawnWreckage()
+    {
+        alive = false;
+        sr.enabled = true;
+    }
+}
